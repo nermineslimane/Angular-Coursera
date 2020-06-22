1b25ea42a62822ac7782e0b956fe1e6ef8ae0428
nermineslimane <slimane.nermine@esprit.tn>
2020-06-22 20:27:52 +0100

    Components part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [AppComponent, MenuComponent],
   imports: [
@@ -17,6 +20,9 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     FlexLayoutModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
